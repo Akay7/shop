941df5c473f2bed8024b2676afd7afc0410a58f1
Poderyagin Egor Sergeyevich <egor@crazyrussian.pro>
2015-04-21 00:55:45 +0300

    Rewrited some tests, more correct work of functional tests, and boiler plate for javascript tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from shop.models import Product
 
 
-class MainPageTest(LiveServerTestCase):
+class BaseFunctionalTest(LiveServerTestCase):
     def setUp(self):
 
         self.product = Product.objects.create(
@@ -21,31 +21,31 @@ class MainPageTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+
+class MainPageTest(BaseFunctionalTest):
     def test_can_going_to_look_details_about_product_from_main_page(self):
         # user come to main page and click on title for looking more detail about product
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_id("product_short_info_1").find_element_by_tag_name("a").click()
+        self.browser.find_element_by_tag_name("a").click()
 
         # user redirected to page with information about product
         info_about_product = self.browser.find_element_by_class_name("Detail").text
         self.assertEqual(info_about_product, self.product.detail)
 
 
-class CartTest(LiveServerTestCase):
+class CartTest(BaseFunctionalTest):
     def test_can_send_product_to_cart_from_main_page(self):
-        self.product = Product.objects.create(
-            title="Product",
-            detail="Very beautiful product. Buy it faster"
-        )
-
         # user can add product to card from main page
-        self.browser = webdriver.Firefox()
         self.browser.get(self.live_server_url)
 
-        #product = self.browser.find_element_by_id("product_short_info_1")
-        #print(product.text)
         elem = self.browser.find_element_by_class_name("add_to_cart")
-        #print(elem.text)
         elem.click()
 
-        self.fail("Write test!!!")
+        # after that user come to cart page and looking for added product
+        self.browser.get(self.live_server_url + "/cart/")
+        self.browser.find_element_by_id("product_short_info_")
+        self.browser.implicitly_wait(40)
+
+
+
+
--- shop/middleware.py
+++ shop/middleware.py
@@ -3,8 +3,11 @@ from .models import Product
 
 class CartMiddleware:
     def process_template_response(self, request, response):
+        '''
         if "cart" not in request.session:
             request.session["cart"] = {}
         response.context_data['cart'] = request.session["cart"]
         #print(response.context_data)
+        '''
+        # TODO: return response with correct data
         return response
